31182bc3d47f09a9cd4bb4e7742d974d97f9617a
barow <w@ba-ro.de>
2025-03-12 18:11:45 +0100

    [TASK] Add composer.json and ext_emconf

---
+++ ext_emconf.php
@@ -0,0 +1,23 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Ter test',
+    'description' => '',
+    'version' => '0.0.1',
+    'category' => 'example',
+    'constraints' => [
+        'depends' => [
+            'php' => '8.1.0-8.4.99',
+            'typo3' => '12.4.26-12.4.99',
+        ],
+    ],
+    'state' => 'alpha',
+    'author' => '',
+    'author_email' => 'typo3@ba-ro.de',
+    'author_company' => '',
+    'autoload' => [
+        'psr-4' => [
+            'Barow\\TerTest\\' => 'Classes/',
+        ],
+    ],
+];
